fc0a32d900c516eede82c2b7470f09cfe17bea5c
dhyana-joshi <dhyana.mayur7e@gmail.com>
2026-08-14 08:36:37 +0530

    Half work done in po34.c
    Fix syntax errors and improve variable handling.

---
+++ po34.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <math.h>
+int main(){
+  int n;
+  int arr[20];
+  printf("Enter a number\n");
+  scanf("%d",&n);
+  int count=0;
+  while(n!=0){
+    c+=1
+    n/=10
+    }
+  for(int i=0,i<=c,i++){
+    
+    
+  return 0
+}
