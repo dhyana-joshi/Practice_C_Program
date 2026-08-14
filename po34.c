969f3e01f43629b2f8b4f99812509cd9dae8a269
dhyana-joshi <dhyana.mayur7e@gmail.com>
2026-08-14 08:55:56 +0530

    Program complete

--- po34.c
+++ po34.c
@@ -7,11 +7,17 @@ int main(){
   scanf("%d",&n);
   int count=0;
   while(n!=0){
-    c+=1
-    n/=10
-    }
+    c+=1;
+   arr[c+1]=n%10;
+    n/=10;
+    } 
+  int k;
   for(int i=0,i<=c,i++){
-    
-    
+    k+=pow(arr[i],c);
+    }
+  if(k==n)
+    printf("Given number is an armstrong number");
+  else
+    printf("Given number is not an armstrong number");
   return 0
 }
